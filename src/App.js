548b0c480e06c61a6b64812256af244c91578937
wdestian <70280320+wdestian@users.noreply.github.com>
2020-08-29 12:17:17 +0700

    input number masih error  ulangi  lagi

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "assets/scss/style.scss";
 import landingPage from "pages/landingPage";
+import Example from "pages/Example";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route path="/" component={landingPage} />
+        <Route path="/example" component={Example} />
       </Router>
     </div>
   );
--- src/elements/Form/InputDate/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import propTypes from "prop-types";
-import "./index.scss";
-
-export default function Number(props) {
-  const { value, placeholder, name, min, max, prefix, suffix } = props;
-  const [InputValue, setInputValue] = useState(`${prefix}${value}${suffix}`);
-  return <div></div>;
-}
